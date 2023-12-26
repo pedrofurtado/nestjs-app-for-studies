327e8ec98934dde7db3269778e56eafaa9d43c59
Pedro Furtado <pedro.furtado@locaweb.com.br>
2023-12-26 10:14:39 -0300

    swaggerzin

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { AppService } from './app.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
 
 async function bootstrap() {
   //Web-based app (with network listeners)
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-aqui', app, document);
+
   await app.listen(3000);
 
   //Standalone app (without network listerners. Ex: rabbitmq consumer, cron jobs, etc)
