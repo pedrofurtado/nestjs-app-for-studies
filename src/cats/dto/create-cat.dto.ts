34b93062001f7f3545c57b8ca50463e8faf2932d
Pedro Furtado <pedro.furtado@locaweb.com.br>
2023-12-26 10:15:38 -0300

    swagger v2

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,18 +1,22 @@
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCatDto {
   // @IsInt()
   // @IsNotEmpty()
   // id: number;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   owner: string;
 
+  @ApiProperty()
   @IsInt()
   @IsNotEmpty()
   age: number;
